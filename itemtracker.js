8a85038525c9af9ccaa8e79f04c444f800e25369
sillypears <stuart.rosenfeld@outlook.com>
2016-01-17 14:22:28 -0500

    Does it work?
    Should shrink if items fill out the allowed window. Someone please test

--- itemtracker.js
+++ itemtracker.js
@@ -18,21 +18,47 @@ function get_items(player) {
  };
 
 function print_response(data) {
-	//console.log(data);
-
+	//console.log(data);s
+	//"Find the length of the amount we are overrunning the div by, divide it equally amongst the items, and move them all closer by that amount."
+	//margin-left = -((numItems * itemPNGWidth) - element.width() / numItems)
+	img_width = 48;
+	img_height = 48;
 	var html = $('<div id=response ></div>');
-	html.append('<table width=100% height=100px id=stuff></table');
+	html.append('<table id=stuff></table');
 	$.each(data, function(x, contents) {
-		html.find('#stuff').append('<tr><td colspan='+ contents.item_list.length+ '>Current seed: '+contents.seed+'</td></tr>');	
-		html.find('#stuff').append('<tr id=items><td></td></tr>');
+		num_items = contents.item_list.length;
+		html.find('#stuff').append('<tr><td colspan='+ num_items + '>Current seed: '+contents.seed+'</td></tr>');	
+		html.find('#stuff').append('<tr id=items_row ><td id=items></td></tr>');
 		$.each(contents.item_list, function(y, object){
-			console.log(object.item_id);
-			html.find('#items').children('td').append('<img width=48px height=48px src=collectibles/collectibles_'+("000" + object.item_id).slice(-3) + '.png />');
+			//console.log(contents);
+			if (object.starting_item){
+				console.log("starting item "+object.item_id);
+			}
+			html.find('#items_row').children('td').append('<img width='+img_width+'px height='+img_height + 'px' +((object.starting_item) ? ' class=starting-item' : ' class=non-start-item') + ' src=collectibles/collectibles_'+("000" + object.item_id).slice(-3) + '.png />');
 		});
 		
 	});
-
+	
 	$('#content').html(html);
+	
+	
+	var table_dimensions = {"width" : $('html').width(), "height" : $('#stuff').height()}
+	console.log(img_width * num_items +  " " +table_dimensions['width']);
+	var non_starts = $('#items').find('.non-start-item');
+	
+	if (img_width * num_items > table_dimensions['width']) {
+		console.log("items are longer " + img_width * num_items +  " " +table_dimensions['width'] + " " + (img_width * num_items - table_dimensions['width']));
+		$.each(non_starts, function(x, item){
+			offset = Math.ceil(( (img_width * num_items + (img_width * num_items * .1)) - table_dimensions["width"]) / num_items) * -1;
+			console.log(offset);
+			$(item).css({"margin-left": offset+"px"});
+		});
+
+	console.log($('#items').width());
+	}
+	$.each(table_dimensions, function(prop, value) {
+		console.log();
+	});
 }
 
 $(document).ready(function() {
@@ -40,9 +66,9 @@ $(document).ready(function() {
 	player = gup('player');
 	if (player != "") {	
 		get_items(player);
-		setInterval(function(){
+		/*setInterval(function(){
 		get_items(player);
-	}, 15000);
+	}, 15000);*/
 		
 	} else {
 		$('#content').html('<div>hi</div>');
