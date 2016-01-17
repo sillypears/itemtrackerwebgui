d26446d2958c75d35265d584eb57a38a0c4369d2
sillypears <stuart.rosenfeld@outlook.com>
2016-01-17 01:59:32 -0500

    Changed some sizings

--- itemtracker.js
+++ itemtracker.js
@@ -21,13 +21,13 @@ function print_response(data) {
 	//console.log(data);
 
 	var html = $('<div id=response ></div>');
-	html.append('<table width=100%  id=stuff></table');
+	html.append('<table width=100% height=100px id=stuff></table');
 	$.each(data, function(x, contents) {
 		html.find('#stuff').append('<tr><td colspan='+ contents.item_list.length+ '>Current seed: '+contents.seed+'</td></tr>');	
 		html.find('#stuff').append('<tr id=items><td></td></tr>');
 		$.each(contents.item_list, function(y, object){
 			console.log(object.item_id);
-			html.find('#items').children('td').append('<img src=collectibles/collectibles_'+("000" + object.item_id).slice(-3) + '.png />');
+			html.find('#items').children('td').append('<img width=48px height=48px src=collectibles/collectibles_'+("000" + object.item_id).slice(-3) + '.png />');
 		});
 		
 	});
